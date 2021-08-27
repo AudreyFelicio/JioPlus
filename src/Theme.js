8448ca9e463b26c636a9018336489f08969e4938
AudreyFelicio <audreyanwar@gmail.com>
2021-08-28 05:54:45 +0800

    Create theme

---
+++ src/Theme.js
@@ -0,0 +1,36 @@
+import { createTheme } from '@material-ui/core';
+import { orange } from './Colors';
+
+export const theme = createTheme({
+  palette: {
+    primary: { main: orange },
+  },
+  typography: {
+    body1: {
+      fontFamily: 'Monserrat',
+    },
+    button: {
+      fontFamily: 'Monserrat',
+      fontWeight: 800,
+    },
+    h2: {
+      fontFamily: 'Monserrat',
+    },
+    h3: {
+      fontFamily: 'Montserrat',
+      fontWeight: 800,
+    },
+    h4: {
+      fontFamily: 'Monserrat',
+      fontWeight: 500,
+    },
+    h5: {
+      fontFamily: 'Monserrat',
+      fontWeight: 500,
+    },
+    body2: {
+      fontFamily: 'Monserrat',
+      fontWeight: 700,
+    },
+  },
+});
